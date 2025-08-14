22f37df9ef4ae2032101626910a3876f2aa0ba86
nikhilpratapsingh23 <pratapsinghnikhil23@gmail.com>
2025-08-14 10:00:58 +0530

    Update task4.py

--- task4.py
+++ task4.py
@@ -10,21 +10,13 @@ def chatbot_response(user_input):
         return "I'm fine, thanks! How about you?"
     elif user_input in ["bye", "goodbye"]:
         return "Goodbye! Have a great day!"
-    elif user_input in ["i like you", "I like you"]:
-        return "me too also"
-    elif user_input in ["do you love me", "Do you love me", "Do You Love me"]:
-        return "Sure ! I Love You"
-    elif user_input in ["What is Your Favorite Colour", "what is your favorite colour"]:
-        return "Pink , Whats your"
-    elif user_input in ["suggest some movie regarding my interest like tech"]:
-        return "Interaseller , Coding With Dady , Genius , Genius2, "
-    elif user_input in ["i am also fine", "I am also fine"]:
-        return "Okay"
+    
+        
     else:
         return "Sorry, I didn't understand that."
 
 def run_chatbot():
-    print("🤖 Basic Chatbot - Type 'bye' to exit.")
+    print(" Basic Chatbot - Type 'bye' to exit.")
     while True:
         user_input = input("You: ")
         response = chatbot_response(user_input)
@@ -34,3 +26,4 @@ def run_chatbot():
 
 if __name__ == "__main__":
     run_chatbot()
+
